a0457241a7fe169a29e0febb755bd9a29eabb487
Jordan <jtaccinelli@gmail.com>
2024-02-10 22:35:48 +1000

    feat: updated server to support new things

--- server.ts
+++ server.ts
@@ -2,12 +2,22 @@ import { logDevReady } from "@remix-run/cloudflare";
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import * as build from "@remix-run/dev/server-build";
 
+import { createPrismaHandler } from "~/lib/prisma";
+import { createSessionHandler } from "~/lib/session";
+import { createSpotifyHandler } from "~/lib/spotify";
+
 if (process.env.NODE_ENV === "development") {
   logDevReady(build);
 }
 
 export const onRequest = createPagesFunctionHandler({
   build,
-  getLoadContext: (context) => ({ env: context.env }),
+  getLoadContext: async ({ request, env }) => {
+    const { session, commitSession } = await createSessionHandler(request);
+    const prisma = await createPrismaHandler();
+    const spotify = await createSpotifyHandler(request, session);
+
+    return { env };
+  },
   mode: build.mode,
 });
