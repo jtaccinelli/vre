8f135c43aa43af85ca68decf120b0b2557ca5b30
Jordan <jtaccinelli@gmail.com>
2024-05-19 23:01:38 +1000

    feat: added index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,35 +1,17 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    {
-      name: "description",
-      content: "Welcome to Remix! Using Vite and Cloudflare!",
-    },
-  ];
-};
-
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex h-full w-full flex-col items-start justify-center gap-4">
+      <p>Enter A Playlist URL</p>
+      <input
+        className="w-full bg-gray-900 px-4 py-2"
+        type="url"
+        name="playlist"
+        id="playlist"
+        required
+      />
+      <button className="rounded-sm bg-emerald-700 px-4 py-2 font-semibold">
+        Get Voting
+      </button>
     </div>
   );
 }
