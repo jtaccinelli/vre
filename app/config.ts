68dea94cac60e9275dc2f194d36ad1fd69a636e8
Jordan <jtaccinelli@gmail.com>
2024-05-22 00:14:07 +1000

    feat: added config file

---
+++ app/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  spotify: {
+    // Endpoints
+    authTokenEndpoint: "https://accounts.spotify.com/api/token",
+    apiEndpoint: "https://api.spotify.com/v1",
+    // Auth Details
+    clientId: "ebe9c3147cae485cbc84fe018fb6281b",
+    clientSecret: "f2624dfb706547ab96a8283e584168f2",
+  },
+};
