4b027b7a62a0671e78cc021e7c07d5b67bd3f1da
Jordan <jtaccinelli@gmail.com>
2024-05-22 00:12:08 +1000

    feat: moved session class to context file, added spotify handler

--- load-context.ts
+++ load-context.ts
@@ -1,6 +1,14 @@
+import { Buffer } from "node:buffer";
+
+import {
+  createCookie,
+  createWorkersKVSessionStorage,
+  Session,
+  SessionStorage,
+} from "@remix-run/cloudflare";
 import { type PlatformProxy } from "wrangler";
 
-import { KVSession } from "./app/lib/session";
+import { config } from "./app/config";
 
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
 
@@ -13,17 +21,136 @@ type GetLoadContextArgs = {
   context: BaseAppLoadContext;
 };
 
+// -- SESSION HANDLER
+
+class KVSession {
+  storage;
+  session;
+
+  constructor(storage: SessionStorage, session: Session) {
+    this.storage = storage;
+    this.session = session;
+  }
+
+  static async init(request: Request, context: BaseAppLoadContext) {
+    const cookie = createCookie("__session", {
+      secrets: ["r3m1xr0ck5"],
+      sameSite: true,
+    });
+
+    const storage = createWorkersKVSessionStorage({
+      kv: context.cloudflare.env.SESSION,
+      cookie: cookie,
+    });
+
+    const session = await storage
+      .getSession(request.headers.get("Cookie"))
+      .catch(() => storage.getSession());
+
+    return new this(storage, session);
+  }
+
+  get has() {
+    return this.session.has;
+  }
+
+  get get() {
+    return this.session.get;
+  }
+
+  get flash() {
+    return this.session.flash;
+  }
+
+  get unset() {
+    return this.session.unset;
+  }
+
+  get set() {
+    return this.session.set;
+  }
+
+  destroy() {
+    return this.storage.destroySession(this.session);
+  }
+
+  commit() {
+    return this.storage.commitSession(this.session);
+  }
+}
+
+// -- SPOTIFY HANDLER
+
+export class Spotify {
+  authToken;
+
+  constructor(authToken: string) {
+    this.authToken = authToken;
+  }
+
+  static async init(session: KVSession) {
+    const sessionToken = session.get("authToken");
+    if (sessionToken) return new this(sessionToken);
+
+    const { clientId, clientSecret, authTokenEndpoint } = config.spotify;
+
+    const authToken = `${clientId}:${clientSecret}`;
+    const buffer = Buffer.from(authToken).toString("base64");
+
+    const body = new URLSearchParams();
+    body.set("grant_type", "client_credentials");
+
+    const headers = new Headers();
+    headers.set("Authorization", `Basic ${buffer}`);
+    headers.set("Content-Type", "application/x-www-form-urlencoded");
+
+    const response = await fetch(authTokenEndpoint, {
+      method: "POST",
+      headers,
+      body: body.toString(),
+    });
+
+    if (response.status !== 200) throw new Error(response.statusText);
+
+    const data = await response.json<{
+      expires_in: string;
+      access_token: string;
+      token_type: string;
+    }>();
+
+    return new this(data.access_token);
+  }
+
+  async fetch(endpoint: string, options: RequestInit = {}) {
+    const { apiEndpoint } = config.spotify;
+
+    const headers = new Headers(options.headers);
+    headers.set("Authorization", `Bearer ${this.authToken}`);
+
+    const url = `${apiEndpoint}${endpoint}`;
+
+    return await fetch(url, { ...options, headers });
+  }
+}
+
+// -- ACTUAL CONTEXT SET UP
+
 declare module "@remix-run/cloudflare" {
   interface AppLoadContext extends BaseAppLoadContext {
     cloudflare: Cloudflare;
-    session: KVSession 
+    session: KVSession;
+    spotify: Spotify;
   }
 }
 
 export async function getLoadContext({ request, context }: GetLoadContextArgs) {
   const session = await KVSession.init(request, context);
+  const spotify = await Spotify.init(session);
+
+  console.log("token", spotify.authToken);
 
   return {
-    session
-  }
+    session,
+    spotify,
+  };
 }
