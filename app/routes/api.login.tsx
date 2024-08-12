d9093914d3043542b0fea50e3376d6c7dbff3dda
Jordan <jtaccinelli@gmail.com>
2024-08-13 00:39:20 +1000

    feat: fixed redirect uri

--- app/routes/api.login.tsx
+++ app/routes/api.login.tsx
@@ -2,11 +2,11 @@
  * This endpoint handles building the login URL for Spotify to allow users to login.
  */
 
-import { redirect } from "@remix-run/cloudflare";
+import { LoaderFunctionArgs, redirect } from "@remix-run/cloudflare";
 
 import { config } from "~/config";
 
-export async function loader() {
+export async function loader({ request }: LoaderFunctionArgs) {
   const { clientId, loginEndpoint } = config.spotify;
 
   const state = (Math.random() + 1).toString(36).substring(7);
@@ -18,11 +18,14 @@ export async function loader() {
     "user-read-email",
   ].join(" ");
 
+  const requestUrl = new URL(request.url);
+  const redirectUri = `${requestUrl.origin}/${config.spotify.redirectUri}`;
+
   const url = new URL(loginEndpoint);
   url.searchParams.set("response_type", "code");
   url.searchParams.set("client_id", clientId);
   url.searchParams.set("scope", scope);
-  url.searchParams.set("redirect_uri", config.spotify.redirectUri);
+  url.searchParams.set("redirect_uri", redirectUri);
   url.searchParams.set("show_dialog", "true");
   url.searchParams.set("state", state);
 
