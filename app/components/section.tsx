8cfe3eefe9ccee796d4bca48896dab0673955b39
Jordan <jtaccinelli@gmail.com>
2025-07-07 02:09:25 +1000

    fix: z-index on home

--- app/components/section.tsx
+++ app/components/section.tsx
@@ -8,7 +8,7 @@ type Props = {
 export function Section({ title, children }: Props) {
   return (
     <div className="flex flex-col pb-10">
-      <p className="heading sticky top-0 border-t border-gray-800 bg-gray-900 p-6">
+      <p className="heading sticky top-0 z-10 border-t border-gray-800 bg-gray-900 p-6">
         {title}
       </p>
       <div className="flex flex-col gap-4 px-6 pt-2">{children}</div>
